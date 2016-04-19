64d08f5db8c962f97a59c4902277cb0549cd2d69
Kyle Cierzan <kcierzan@gmail.com>
2016-04-19 13:57:11 -0400

    Add confirmation for email and password

--- lib/clea.rb
+++ lib/clea.rb
@@ -6,50 +6,26 @@ require "ValidateEmail"
 module Clea
   class Sender
 
-    def send_message
-      # Initialize the gmail SMTP connection
-      smtp = Net::SMTP.new('smtp.gmail.com', 587)
-      #Upgrade connection to SSL/TLS
-      smtp.enable_starttls
 
-      # Open SMTP connection, send message, close the connection
-      smtp.start('gmail.com', user_info[:from_address], user_info[:password], :login) do
-        smtp.send_message(msg, user_info[:from_address], recipient)
-      end
-    end
-
-    def fetch_and_compose
-      # Read from persistent user_info hash and assign variables
-
-
-      # Compose message
-      msg = <<-END_OF_MESSAGE
-      From: #{user_info[:from_alias]} <#{user_info[:from_address]}>
-      To: <#{recipient}>
-      Subject: #{subject_line}
-
-      #{body}
-      END_OF_MESSAGE
-    end
-
-    def get_info
+    # Retrieve valid info from the persistent hash or from user input
+    def get_sender_info
       # Initialize or open persistent user info hash
-      @user_info = PStore.new("user-info.pstore")
+      @sender_info = PStore.new("my-clea-info.pstore")
 
       # If there is no persisting value for from_address, get values for alias, from_address, and password
-      if user_info[:from_address] == nil
+      if sender_info[:from_address] == nil
         puts "What is your name?"
         from_alias = gets.chomp
 
-
-        puts "Enter your GMail address:"
+        # Validate and confirm from_address
+        puts "Enter your gmail address:"
         while from_address = gets.chomp
-          catch :unconfirmed do
+          catch :badfrom do
             case from_address
             when ValidateEmail.validate(from_address, true) == true
               puts "Is your email address #{from_address}? [y/n]:"
               while confirmation = gets.chomp
-                throw :unconfirmed unless confirmation == 'y'
+                throw :badfrom unless confirmation == 'y'
                 break
               end
             else
@@ -58,16 +34,35 @@ module Clea
           end
         end
 
+        # Get user password and confirm
+        confirmed = false
         puts "Enter your password:"
-        password = gets.chomp
+        while password = gets.chomp && confirmed == false
+          catch :badpass do
+            puts "Are you sure your password is #{password}? [y/n]:"
+            while confirmation = gets.chomp
+              case confirmation
+              when 'y'
+                confirmed = true
+                break
+              else
+                "Please re-enter your password:"
+                throw :badpass
+              end
+            end
+          end
+        end
+
+
+
 
         # Write input password, from_address, and from_alias to persistent hash
-        user_info.transaction do
-          user_info[:password] = password
+        sender_info.transaction do
+          sender_info[:password] = password
           # Abort user info storage if the entered from_address is invalid
-          # user_info.abort unless ValidateEmail.validate(from_address, true) == true
-          user_info[:from_address] = from_address
-          user_info[:from_alias] = from_alias
+          # sender_info.abort unless ValidateEmail.validate(from_address, true) == true
+          sender_info[:from_address] = from_address
+          sender_info[:from_alias] = from_alias
         end
       else
         puts "Enter the recipient's Email address"
@@ -82,5 +77,34 @@ module Clea
         end
       end
     end
+
+    # Interpolate user info into message string
+    def compose
+      # Read from persistent sender_info hash and assign variables
+
+
+      # Compose message
+      msg = <<-END_OF_MESSAGE
+      From: #{sender_info[:from_alias]} <#{sender_info[:from_address]}>
+      To: <#{recipient}>
+      Subject: #{subject_line}
+
+      #{body}
+      END_OF_MESSAGE
+    end
+
+    # Open SMTP connection, pass user info as arguments, send message and close connection
+    def send_message
+      # Initialize the gmail SMTP connection
+      smtp = Net::SMTP.new('smtp.gmail.com', 587)
+      #Upgrade connection to SSL/TLS
+      smtp.enable_starttls
+
+      # Open SMTP connection, send message, close the connection
+      smtp.start('gmail.com', sender_info[:from_address], sender_info[:password], :login) do
+        smtp.send_message(msg, sender_info[:from_address], recipient)
+      end
+    end
+
   end
 end
