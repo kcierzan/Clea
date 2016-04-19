3dfc519c578cac2f36c5251dcaf89ad37c7b480d
Kyle Cierzan <kcierzan@gmail.com>
2016-04-19 15:11:16 -0400

    Remove syntax errors

--- lib/clea.rb
+++ lib/clea.rb
@@ -6,26 +6,43 @@ require "ValidateEmail"
 module Clea
   class Sender
 
-
     # Retrieve valid info from the persistent hash or from user input
-    def get_sender_info
+    def write_sender_info
       # Initialize or open persistent user info hash
       @sender_info = PStore.new("my-clea-info.pstore")
 
       # If there is no persisting value for from_address, get values for alias, from_address, and password
-      if sender_info[:from_address] == nil
-        puts "What is your name?"
-        from_alias = gets.chomp
+      senders_from = @sender_info.transaction { sender_info[:from_address] }
 
-        # Validate and confirm from_address
+      if senders_from == nil
+        # Get user's alias and confirm
+        alias_confirmed = false
+        puts "What is your name?"
+        while from_alias = gets.chomp
+          catch :badalias do
+            puts "Are you sure your name is #{from_alias}? [y/n]:"
+            while alias_confirmation = gets.chomp
+              case alias_confirmation
+              when 'y'
+                alias_confirmed = true
+                break
+              else
+                puts "Please re-enter your name:"
+                throw :badalias
+              end
+            end
+          end
+        end
+
+        # Get user's email address, validate, confirm
         puts "Enter your gmail address:"
         while from_address = gets.chomp
           catch :badfrom do
             case from_address
             when ValidateEmail.validate(from_address, true) == true
               puts "Is your email address #{from_address}? [y/n]:"
-              while confirmation = gets.chomp
-                throw :badfrom unless confirmation == 'y'
+              while address_confirmation = gets.chomp
+                throw :badfrom unless address_confirmation == 'y'
                 break
               end
             else
@@ -34,27 +51,24 @@ module Clea
           end
         end
 
-        # Get user password and confirm
-        confirmed = false
+        # Get user's password and confirm
+        pass_confirmed = false
         puts "Enter your password:"
         while password = gets.chomp && confirmed == false
           catch :badpass do
             puts "Are you sure your password is #{password}? [y/n]:"
-            while confirmation = gets.chomp
-              case confirmation
+            while pass_confirmation = gets.chomp
+              case pass_confirmation
               when 'y'
-                confirmed = true
+                pass_confirmed = true
                 break
               else
-                "Please re-enter your password:"
+                puts "Please re-enter your password:"
                 throw :badpass
               end
             end
           end
         end
-
-
-
 
         # Write input password, from_address, and from_alias to persistent hash
         sender_info.transaction do
@@ -64,19 +78,34 @@ module Clea
           sender_info[:from_address] = from_address
           sender_info[:from_alias] = from_alias
         end
-      else
-        puts "Enter the recipient's Email address"
-        @recipient = gets.chomp
-
-        if ValidateEmail.validate(@recipient, true) == false
-          puts "What is the subject of the email?"
-          @subject_line = gets.chomp
-
-          puts "Enter your message:"
-          @body = gets.chomp
-        end
       end
     end
+
+    def get_msg_info
+      puts "Enter the recipient's email address:"
+      while @to_address = gets.chomp
+        catch :badto do
+          case to_address
+          when ValidateEmail.validate(to_address, true) == true
+            puts "Is the recipient's email address #{to_address}? [y/n]:"
+            while to_address_confirmation = gets.chomp
+              throw :badto unless to_address_confirmation == 'y'
+              break
+            end
+          else
+            puts "Email invalid! Please enter a valid email address:"
+          end
+        end
+      end
+
+      puts "What is the subject of the email?"
+      @subject_line = gets.chomp
+
+      puts "Enter your message:"
+      @body = gets.chomp
+    end
+
+
 
     # Interpolate user info into message string
     def compose
@@ -84,24 +113,27 @@ module Clea
 
 
       # Compose message
-      msg = <<-END_OF_MESSAGE
+      @msg = <<-END_OF_MESSAGE
       From: #{sender_info[:from_alias]} <#{sender_info[:from_address]}>
-      To: <#{recipient}>
-      Subject: #{subject_line}
+      To: <#{@to_address}>
+      Subject: #{@subject_line}
 
-      #{body}
+      #{@body}
       END_OF_MESSAGE
     end
 
     # Open SMTP connection, pass user info as arguments, send message and close connection
     def send_message
+      # Read the user's password from the persistent hash
+      password = @sender_info.transaction { sender_info[:password] }
       # Initialize the gmail SMTP connection
       smtp = Net::SMTP.new('smtp.gmail.com', 587)
       #Upgrade connection to SSL/TLS
       smtp.enable_starttls
 
+
       # Open SMTP connection, send message, close the connection
-      smtp.start('gmail.com', sender_info[:from_address], sender_info[:password], :login) do
+      smtp.start('gmail.com', sender_info[:from_address], password, :login) do
         smtp.send_message(msg, sender_info[:from_address], recipient)
       end
     end
